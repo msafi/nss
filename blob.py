01147d9c3ccd7fb25383fa2d154fcd7b9dc1e8d9
M.K. Safi <msafi@msafi.com>
2024-10-14 20:06:36 -0700

    Co-authored-by: DragonBlade4419 <DragonBlade4419@users.noreply.github.com>

--- blob.py
+++ blob.py
@@ -1,15 +1,17 @@
-#import threeSidedDie from die.py
+# import threeSidedDie from die.py
 from die import Die
+
 
 # blob class
 class Blob:
     die = Die(3)
     areadie = Die(100)
+
     def __init__(self, name, size):
         self.die_value = self.die.roll()
         self.name = name
         self.areadie_value = self.areadie.roll
-
+        # hibity hopity
         if self.areadie_value == 1:
             self.size = size - 1
         elif self.die_value == 2:
@@ -25,12 +27,15 @@ class Blob:
 
     def roll_die(self):
         self.die_value = self.die.roll()
-    
+
     def roll_areadie(self):
         self.areadie_value = self.areadie.roll
 
     def __str__(self):
-        return f"Blob(name={self.name}, size={self.size}, location={self.areadie_value})"
+        return (
+            f"Blob(name={self.name}, size={self.size}, location={self.areadie_value})"
+        )
+
     def adjust_size(self):
         if self.die_value == 1:
             self.size = self.size - 1
@@ -38,5 +43,3 @@ class Blob:
             self.size = self.size
         else:
             self.size = self.size + 1
-
-
